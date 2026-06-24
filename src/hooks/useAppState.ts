3cd824288077761384bc95cdec861f8ad9d7d410
Ismail Cankaya <ismayilcankaya2017@gmail.com>
2026-06-24 23:09:05 +0300

    fix error

--- src/hooks/useAppState.ts
+++ src/hooks/useAppState.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Invitation, RSVPResponse } from '../types';
 import { INITIAL_INVITATION, INITIAL_RSVP_LIST } from '../data';
 
